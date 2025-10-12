9a8f2f61194961915f21cb8a3734be02d37697dd
stringtheorist <aftab26989@gmail.com>
2025-10-12 21:42:13 +0200

    Files for TP Oct 13

--- f1.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = f1(inputArg1,inputArg2)
-%F1 Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
