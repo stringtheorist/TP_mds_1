64958175ffd4a9de2d6a041b2f35d44b071e5bb2
Daphne-Aliprantis <daphnealiprantis@gmail.com>
2025-10-13 11:12:25 +0200

    Illustration pas fini

--- Illustration.m
+++ Illustration.m
@@ -1,1 +1,25 @@
 function Illustration(Type,u,s,t)
+% 
+% %=> visualisation de quelques modes propres
+% figure(1);
+% plot(s,Y([1:3 nmax],:),'LineWidth',2)
+% xlabel('s [m]')
+% legend('n=1','n=2','n=3','n=nmax')
+% set(gca,'FontSize',24)
+% 
+% % Amplitude modale
+% 
+% %=> visualisation des amplitudes modales an
+% figure(2);
+% stem(wn,abs(an),'LineWidth',2)
+% xlabel('wn [rad/s]')
+% ylabel('|an| [m]')
+% set(gca,'FontSize',24)
+% 
+% % Fonction en temps
+% %=> visualisation de T(t) pour quelques modes
+% figure(3);
+% plot(t,T([1:3 nmax],:),'LineWidth',2)
+% xlabel('t [s]')
+% legend('n=1','n=2','n=3','n=nmax')
+% set(gca,'FontSize',24)
