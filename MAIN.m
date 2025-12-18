fbfe139b1d43f7bb7b689c6c7c7cf2e83b1c0397
mcguery <marie-clemence.guery@etudiant.univ-rennes.fr>
2025-12-18 15:24:32 +0100

    ressort a gauche

--- MAIN.m
+++ MAIN.m
@@ -17,6 +17,18 @@ nmax=10;
 % raisonnables (<=1000) et si possible distinctes.
 disp(['[Nt,Ns,Nw]=[' num2str([Nt,Ns,Nw]) ']'])
 
+
+% Excitation: impacte ponctuel en s=el 
+V=C*2;          % Vitesse de l'impact [m/]
+el=L/6;         % Position de l'impact[m]
+K=5.1*pi*N0/L;  % Raideur du ressort appliqué en s=L [N/m]
+% Domaine modal
+nmax=10;        % Nombre maximal de mode considéré      
+n=(1:nmax)';    % Indices modaux
+
+
+
+
 %% ========================================================================
 %% ANALYSE MODALE =========================================================
 % Modes propres
@@ -36,6 +48,29 @@ u=FctDeplacement(Y,T);
 %Film(u,s,Nt,L,H)
 
 
+%% ========================================================================
+%% ANALYSE MODALE =========================================================
+% Modes propres
+for in=1:nmax
+    % Y_ij, avec i=>n et j=>s
+Y(in,:)=cos(kn(in).*s)+K/N0*sin(kn(in).*s)./kn(in);    % Y_ij, avec i=>n et j=>s
+% Norme au carré de chaque mode (obtenu via calcul formel) 
+normY2(in,:)=(2*(K^2.*kn(in)*L+K*N0+kn(in)*L*N0^2)-2*K*N0*cos(2*kn(in)*L)+(N0^2-K^2).*sin(2*kn(in)*L))./(4*kn(in)*N0^2);
+end
+%-> visualisation de quelques modes propres
+figure(5);
+plot(s,Y([1:3 nmax],:),'LineWidth',2)
+xlabel('s [m]')
+legend('n=1','n=2','n=3','n=nmax')
+set(gca,'FontSize',24)
+% Amplitude modale
+[an,bn]=AmplitudeModale(L,el,kn,wn,n,H,Aff);
+% Fonction en temps
+T=FctTemporelle(Nw,wn,an,bn,t,Aff);
+% Deplacement
+u=FctDeplacement(Y,T);
+
+Type=3;Illustration(Type,u,s,t,Nt,L,H)
 
 % D'autres valorisations peuvent etre envisagees, quelques propostion
 % Film ?
